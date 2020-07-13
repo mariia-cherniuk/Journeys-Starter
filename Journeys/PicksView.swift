71b74354b77d3891d19b723da54b30ca277f23e2
Mariia Cherniuk <marydort1@gmail.com>
2020-07-13 16:29:11 +0100

    LazyVGrid

--- Journeys/PicksView.swift
+++ Journeys/PicksView.swift
@@ -11,6 +11,10 @@ struct PicksView: View {
     
     @EnvironmentObject var locations: Locations
     
+    private let layout = [
+        GridItem(.adaptive(minimum: 100))
+    ]
+    
     var body: some View {
         GeometryReader { geo in
             ScrollView {
@@ -30,6 +34,34 @@ struct PicksView: View {
                 .frame(height: 300)
                 .tabViewStyle(PageTabViewStyle())
                 .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+                
+                LazyVGrid(columns: layout, spacing: 20) {
+                    ForEach(locations.places) { place in
+                        NavigationLink(destination: DiscoverView(location: place)) {
+                            VStack {
+                                Image(place.heroPicture)
+                                    .resizable()
+                                    .frame(height: 125)
+                                    .overlay(
+                                        VStack {
+                                            Spacer()
+                                            HStack {
+                                                Text(place.name)
+                                                    .font(.caption)
+                                                    .padding(8)
+                                                    .foregroundColor(.white)
+                                                Spacer()
+                                            }
+                                            .background(Color.black.opacity(0.2))
+                                        }
+                                    )
+                                    .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                            }
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                }
+                .padding(20)
             }
         }
         .navigationTitle("Our Top Picks")
