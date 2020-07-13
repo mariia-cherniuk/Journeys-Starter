106502507da39f579e9ec8e4098c134af5e9bea1
Mariia Cherniuk <marydort1@gmail.com>
2020-07-13 14:38:14 +0100

    TabView

--- Journeys/PicksView.swift
+++ Journeys/PicksView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct PicksView: View {
+    
     @EnvironmentObject var locations: Locations
-
+    
     var body: some View {
-        Text("Hello, World!").padding()
-            .navigationTitle("Our Top Picks")
+        GeometryReader { geo in
+            ScrollView {
+                TabView {
+                    ForEach(locations.primary.pictures, id: \.self) { picture in
+                        VStack {
+                            Image(picture)
+                                .resizable()
+                                .scaledToFit()
+                                .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                                .frame(width: geo.size.width, height: 250)
+                            
+                            Spacer()
+                        }
+                    }
+                }
+                .frame(height: 300)
+                .tabViewStyle(PageTabViewStyle())
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            }
+        }
+        .navigationTitle("Our Top Picks")
     }
 }
 
